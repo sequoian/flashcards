a885088aad6ba3996f2dead565bc92464a33e50f
Zach Askins <zach303@msn.com>
2017-06-07 15:59:42 -0700

    basic state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const flashcards = [
+const c = [
   {
     title: 'Regular Expressions',
     cards: [
       {
-        front: '\w',
+        front: '\\w',
         back: 'word character'
       },
       {
-        front: '\d',
+        front: '\\d',
         back: 'digit'
       },
       {
@@ -40,30 +40,72 @@ const flashcards = [
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flashcards: [],
+      activeSetIndex: null
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      flashcards: c,  // should pull from json file(s)
+      activeSetIndex: 0  // for testing
+    });
+  }
+
   render() {
+    const {flashcards, activeSetIndex} = this.state;
+    let page = null;
+    if (flashcards[activeSetIndex]) {
+      page = (
+        <CardPage 
+          set={flashcards[activeSetIndex]}
+        />
+      );
+    }
+    else {
+      page = 'Could not find flashcards.';
+    }
     return (
       <div className="App">
-        <CardPage 
-          set={flashcards[0]}
-        />
+        {page}
       </div>
     );
   }
 }
 
 class CardPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeCardIndex: 0,
+      showingFront: true
+    }
+  }
+
   render() {
     const {set} = this.props;
+    const {activeCardIndex, showingFront} = this.state
+    const activeCard = set.cards[activeCardIndex];
+    let face = null;
+    if (activeCard) {
+      face = showingFront ? activeCard.front : activeCard.back;
+    }
+    else {
+      face = 'Could not find card';
+    }
     return (
       <div>
         <CardInfo
           setTitle={set.title}
           setLength={set.cards.length}
-          cardIndex={2}
-          cardSide={false}
+          cardIndex={activeCardIndex}
+          cardSide={showingFront}
         />
         <Card
-          face={set.cards[2].back}
+          face={face}
         />
         <CardControls />
       </div>
@@ -77,7 +119,7 @@ const CardControls = ({getPrevious, getNext, flipCard}) => (
     <button onClick={flipCard}>Flip</button>
     <button onClick={getNext}>Next</button>
   </div>
-)
+);
 
 const CardInfo = ({setTitle, setLength, cardIndex, cardSide}) => (
   <div>
@@ -85,12 +127,12 @@ const CardInfo = ({setTitle, setLength, cardIndex, cardSide}) => (
     <div>{cardIndex + 1}/{setLength}</div>
     <div>{cardSide ? 'Front' : 'Back'}</div>
   </div>
-)
+);
 
 const Card = ({face}) => (
   <div>
     {face}
   </div>
-)
+);
 
 export default App;
